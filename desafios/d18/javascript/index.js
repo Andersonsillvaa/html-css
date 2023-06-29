ea8992fa3b9a5c9569db576fc0a8a2e936f7c6e1
andersonsillvaa <126997814+Andersonsillvaa@users.noreply.github.com>
2023-06-28 23:37:38 -0300

    Atualizei

---
+++ desafios/d18/javascript/index.js
@@ -0,0 +1,17 @@
+var corpo = document.body
+function escuro() {
+    var esc = document.getElementById('escuro')
+    var inp = document.getElementById('nome')
+    var inp2 = document.getElementById('sobre')
+    corpo.style.background = 'gray'
+    corpo.style.color = 'white'
+    esc.style.background = 'red'
+    inp.style.color = 'black'
+    inp2.style.color = 'black'
+
+}
+
+function claro() {
+    var claro = document.getElementById('escuro1')
+    corpo.style.background = 'white'
+}
